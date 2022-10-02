ef141d143f3b92b4a44e71a7bef1be79d43f71f1
dtsivkovski <dtsivkovs@gmail.com>
2022-10-01 23:36:21 -0700

    added seoul api

---
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/CityAPI.java
@@ -0,0 +1,61 @@
+package com.nighthawk.spring_portfolio.controllers;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Date;
+import java.util.HashMap;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController // annotation to create a RESTful web services
+@RequestMapping("/cityapi")  //prefix of API
+public class CityAPI {
+    private JSONObject body; //last run result
+    private HttpStatus status; //last run status
+    String last_run = null; //last run day of month
+
+    // GET Covid 19 Stats
+    @GetMapping("/seouldaily")   //added to end of prefix as endpoint
+    public ResponseEntity<JSONObject> getCityData() {
+
+        //calls API once a day, sets body and status properties
+        String today = new Date().toString().substring(0,10); 
+        if (last_run == null || !today.equals(last_run))
+        {
+            try {  //APIs can fail (ie Internet or Service down)
+
+                //RapidAPI header
+                HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://wft-geo-db.p.rapidapi.com/v1/geo/cities/Q8684"))
+                    .header("X-RapidAPI-Key", "cd81236483mshbc05c3041f1ca4cp1cfad3jsnb28e0b499ace")
+                    .header("X-RapidAPI-Host", "wft-geo-db.p.rapidapi.com")
+                    .method("GET", HttpRequest.BodyPublishers.noBody())
+                    .build();
+                HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+                //JSONParser extracts text body and parses to JSONObject
+                this.body = (JSONObject) new JSONParser().parse(response.body());
+                this.status = HttpStatus.OK;  //200 success
+                this.last_run = today;
+            }
+            catch (Exception e) {  //capture failure info
+                HashMap<String, String> status = new HashMap<>();
+                status.put("status", "RapidApi failure: " + e);
+
+                //Setup object for error
+                this.body = (JSONObject) status;
+                this.status = HttpStatus.INTERNAL_SERVER_ERROR; //500 error
+                this.last_run = null;
+            }
+        }
+
+        //return JSONObject in RESTful style
+        return new ResponseEntity<>(body, status);
+    }
+}
