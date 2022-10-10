8341fa8154bd52068914c82577dcb48f28f4d7c1
dtsivkovski <dtsivkovs@gmail.com>
2022-10-09 20:39:51 -0700

    songs api

--- src/main/java/com/nighthawk/spring_portfolio/mvc/songs/Songs.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/songs/Songs.java
@@ -1,4 +1,4 @@
-package com.nighthawk.spring_portfolio.mvc.jokes;
+package com.nighthawk.spring_portfolio.mvc.songs;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,11 +10,15 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity // Annotation to simplify creating an entity, which is a lightweight persistence domain object. Typically, an entity represents a table in a relational database, and each entity instance corresponds to a row in that table.
-public class Jokes {
+public class Songs {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(unique=true)
-    private String joke;
+    private String song;
+    
+    private int haha;
+    private int boohoo;
+    
 }
